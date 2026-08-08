4b6e905817f2d209aec99966fd742f2780ad9827
SarrMouhamadu <sarrmahmoud232@gmail.com>
2026-08-08 15:21:20 +0000

    Initial Commit

---
+++ AL Bourda/AL_BourdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AL_BourdaApp.swift
+//  AL Bourda
+//
+//  Created by Mouhamadou SARR on 08-08-2026.
+//
+
+import SwiftUI
+
+@main
+struct AL_BourdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
